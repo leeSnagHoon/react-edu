39c53264410c6f9b12ccef2053dcbe0749a95c64
hoonlee <ums722@naver.com>
2021-01-29 09:52:10 +0900

    add databaseConnect

--- server.js
+++ server.js
@@ -3,42 +3,40 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const fs = require('fs')
+const data = fs.readFileSync('./database.json')
+const conf = JSON.parse(data)
+const mysql = require('mysql')
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}))
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+
+connection.connect()
 
 app.get('/hello', (req, res) =>{
     res.send({message: "hello"})
 })
 
 app.get('/api/customers', (req, res) => {
+
+    connection.query(
+        "select * from customer",
+        (err, row, fields) => {
+            console.log("row : " + JSON.stringify(row))
+
+            res.send(row)
+        }
+    )
     console.debug("call customers")
-    res.send([{
-        'id':'1',
-        'image':'https://placeimg.com/64/64/1',
-        'name': 'jone',
-        'birthday': '20083229',
-        'gender': 'male',
-        'job': 'student'
-
-    },{
-        'id':'2',
-        'image':'https://placeimg.com/64/64/2',
-        'name': 'tom',
-        'birthday': '20083231',
-        'gender': 'female',
-        'job': 'student'
-
-    },{
-        'id':'3',
-        'image':'https://placeimg.com/64/64/3',
-        'name': 'lee',
-        'birthday': '20083230',
-        'gender': 'male',
-        'job': 'teacher'
-
-    }])
 })
 
 
