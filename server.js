b770bd7d9681938cb128f003088b0f276162388c
hoonlee <ums722@naver.com>
2021-03-10 10:17:32 +0900

    add extra

--- server.js
+++ server.js
@@ -1,48 +1,100 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+
 const app = express();
 const port = process.env.PORT || 8081;
 const cors = require('cors');
-const fs = require('fs')
-const data = fs.readFileSync('./database.json')
-const conf = JSON.parse(data)
-const mysql = require('mysql')
+
 const multer = require('multer')
 const upload = multer({dest: './upload'})
+const errorController = require('./controllers/errorControllers')
+const db = require('./database/mysql-connect')
+const users = require('./controllers/users')
+const {auth} = require('./middleware/auth')
 
+
+
+db.connect()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
+app.use(cookieParser())
 
 
 app.use('/image', express.static('./upload'))
-app.post('/api/customers', upload.single('image'), (req, res) => {
-    let sql = 'Inset into customer values (null, ?,?,?,?,?)'
-    let image = '/image/'+req.file.fileName
+app.post('/api/customers', upload.single('image'), async (req, res) => {
+
+    console.debug( "call post /api/customers" )
+
+    let sql = 'Insert into customer values (null, ?, ?, ?, ?, ?, now(), 0)'
+    let image = 'http://localhost:8081/image/'+req.file.filename
     let name = req.body.name
     let birthday = req.body.birthday
     let gender = req.body.gender
     let job = req.body.job
+
     let params = [image, name, birthday, gender, job]
-    connection.query(sql, params, (err, rows, fields) => {
-        res.send(rows)
-    })
+    let input = {
+        'sql':sql,
+        'params':params
+    }
+    try {
+        let result = await db.query(input)
+        console.log('result : '+result)
+        res.send(result)
+    }
+    catch (err){
+        console.log("err " + err)
+    }
+
+    // db.query(input, (err, rows) => {
+    //     console.debug(err)
+    //     console.debug("rows : "+rows)
+    //
+    //     res.send(rows)
+    // })
 
 })
 
-const connection = mysql.createConnection({
-    host: conf.host,
-    user: conf.user,
-    password: conf.password,
-    port: conf.port,
-    database: conf.database
+app.get ('/api/customers', async (req, res) =>  {
+
+    let sql = 'select * from customer where isDeleted = 0'
+    let input = {
+        'sql':sql
+    }
+
+    try {
+        let result = await db.query(input)
+        console.log('a : '+result)
+        res.send(result)
+    }
+    catch (err){
+        console.log("err " + err)
+    }
+
+    console.log("call customers")
 })
 
-connection.connect()
+app.delete('/api/customers/:id', async (req, res) => {
+    console.log("delete")
+    let sql = "update customer set isDeleted = 1 where id = ?"
+    let input = {
+        'sql':sql,
+        'params':parseInt(req.params.id)
+    }
 
-app.get('/hello', (req, res) =>{
-    res.send({message: "hello"})
+    try {
+        let result = await db.query(input)
+        console.log('delete result : '+result)
+        res.send(result)
+    }
+    catch (err){
+        console.log("delete err " + err)
+    }
 
+    console.log("call delete")
 })
 
 
@@ -51,51 +103,45 @@ app.get('/map', (req, res) =>{
 
 })
 
-app.get('/sign-up', (req, res) =>{
 
-    signUp(req, res)
+app.get('/api/idCheck/:user_id', (req, res) =>{
+    console.log('idCheck req : ' + JSON.stringify(req.params))
+    users.integrityCheck(req, res)
+
 })
 
-async function signUp(req, res) {
-    const  {password, salt} = await createHashedPassword(req.body.user.password);
-    connection.query(
-        "select * from customer",
-        (err, row, fields) => {
-            console.log("row : " + JSON.stringify(row))
+app.post('/api/register', (req, res) =>{
+    console.log('register req : ' + JSON.stringify(req.body))
+    users.signUp(req, res)
 
-            res.send(row)
-        }
-    )
-}
-
-app.get('/api/customers', (req, res) => {
-
-    connection.query(
-        "select * from customer",
-        (err, row, fields) => {
-            console.log("row : " + JSON.stringify(row))
-
-            res.send(row)
-        }
-    )
-    console.log("call customers")
 })
 
-const  createHashedPassword = (plainPassword) =>
-    new Promise(async (resolve, reject) => {
-        const salt = await createSalt();
-        crypto.pbkdf2(plainPassword, salt, 9999, 64, 'sha512', (err, key) => {
-            if (err) reject(err);
-            resolve({ password: key.toString('base64'), salt });
-        });
-    });
+app.post('/api/login', (req, res) =>{
+    console.log('register req : ' + JSON.stringify(req.body))
 
-const createSalt = () =>
-    new Promise((resolve, reject) => {
-        crypto.randomBytes(64, (err, buf) => {
-            if (err) reject(err);
-            resolve(buf.toString('base64'));
-        });
-    });
+    users.login(req, res)
 
+})
+
+app.get('/api/logout', (req, res) =>{
+    console.log('logout req : ' + JSON.stringify(req.body))
+
+    users.logout(req, res)
+
+})
+
+app.get('/api/auth', auth, (req, res) =>{
+    let value = {
+        _id:req.user_id,
+        isAuth: true
+    }
+    res.send(value)
+    console.log('/api/auth ')
+
+    //auth.auth(req, res)
+
+})
+
+app.use(errorController.pageNotFoundError);
+app.use(errorController.respondInternalError);
 app.listen(port, () => console.log('Listening on port ' + port))
